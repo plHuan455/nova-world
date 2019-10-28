d22e2bd492f1fcf0adc22413b01daf3e9d30fdee
Khoa Nguyen <khoanguyen@3forcom.com>
2019-10-28 10:08:08 +0700

    Add StoryShots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.tsx$/), module);
+configure(requireContext('../src/stories', true, /\.stories\.tsx$/), module);
