62eda1848ab75ef66a5265127cb985eb510226f5
Khoa Nguyen <khoanguyen@3forcom.com>
2019-10-30 18:56:47 +0700

    Fix Storybook demo type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,3 +6,17 @@ declare module "paths.macro" {
 
   export const filename: string;
 }
+
+declare module "@storybook/react/demo" {
+  interface WelcomeProps {
+    showApp(...args: any[]): any;
+  }
+
+  interface ButtonProps {
+    children: React.ReactChild;
+    onClick(...args: any[]): any;
+  }
+
+  export const Welcome: React.ComponentType<WelcomeProps>;
+  export const Button: React.ComponentType<ButtonProps>;
+}
