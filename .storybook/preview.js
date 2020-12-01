f369518eacc536148a7cabdb8cd7a8c180f435be
Khoa Nguyen <khoanguyen@3forcom.com>
2020-12-01 10:15:39 +0700

    :sparkles: storybook-addon-html

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,12 @@
+import { withHTML } from '@whitespace/storybook-addon-html/react';
+
 import '!style-loader!css-loader!sass-loader!../src/index.scss';
 import '!style-loader!css-loader!sass-loader!../src/App.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [
+  withHTML(),
+];
