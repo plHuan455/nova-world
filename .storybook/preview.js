aa56bd87322f08fafc577e84dd80aae59c17606a
Khoa Nguyen <khoanguyen@3forcom.com>
2020-12-01 10:28:53 +0700

    :hammer: addon-storyshots

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,10 @@
 import { withHTML } from '@whitespace/storybook-addon-html/react';
 
-import '!style-loader!css-loader!sass-loader!../src/index.scss';
-import '!style-loader!css-loader!sass-loader!../src/App.scss';
+if (typeof jest === "undefined") {
+  // NOTES: Resolve storybook static file after built
+  require('!style-loader!css-loader!sass-loader!../src/index.scss');
+  require('!style-loader!css-loader!sass-loader!../src/App.scss');
+}
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
