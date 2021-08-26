e03906c82c062381647cbabab7698b1cbd919732
tiennguyenminh <tien.nguyen@3forcom.com>
2021-08-26 23:02:19 +0700

    :zap:intro commponent

---
+++ src/components/organisms/Tabs/index.stories.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { Story, Meta } from '@storybook/react';
+import React, { useState } from 'react';
+
+import Tabs, { Panel, Tab } from '.';
+
+export default {
+  title: 'Components/organisms/Tabs',
+  component: Tabs,
+  argTypes: {},
+} as Meta;
+
+const listLabel = ['Tin dự án', 'Tin thị trường', 'Tin tập đoàn'];
+const listPanel = ['Nội dung tin dự án', 'Nội dung tin thị trường', 'Nội dung tin tập đoàn'];
+
+export const normal: Story = () => {
+  const [indexActive, setIndexActive] = useState(0);
+  return (
+    <div style={{
+      padding: '50px 0',
+      backgroundColor: '#269947',
+    }}
+    >
+      <Tabs slidesToShow={listLabel.length}>
+        {listLabel.map((item, index) => (
+          <Tab
+            key={`tab-${index.toString()}`}
+            active={index === indexActive}
+            label={item}
+            handleClick={() => setIndexActive(index)}
+          />
+        ))}
+      </Tabs>
+
+      <br />
+
+      {listPanel.map((item, index) => (
+        <Panel
+          key={`panel-${index.toString()}`}
+          active={indexActive === index}
+        >
+          {item}
+        </Panel>
+      ))}
+    </div>
+  );
+};
---
+++ src/components/organisms/Tabs/index.tsx
@@ -0,0 +1,97 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect, useRef } from 'react';
+import Slider, { ResponsiveObject } from 'react-slick';
+
+import Text from 'components/atoms/Text';
+import mapModifiers from 'utils/functions';
+
+interface TabsProps {
+  responsive?: ResponsiveObject[];
+  tabActive?: number;
+  slidesToShow?: number;
+}
+
+interface TabProps {
+  label?: string;
+  active?: boolean;
+  handleClick?: () => void;
+}
+
+interface PanelProps {
+  active?: boolean;
+}
+
+export const Panel: React.FC<PanelProps> = ({
+  active,
+  children,
+}) => (
+  <div className={mapModifiers('o-tabs_panel', active && 'active')}>
+    {children}
+  </div>
+);
+
+export const Tab: React.FC<TabProps> = ({
+  label,
+  active,
+  handleClick,
+}) => (
+  <div
+    onClick={handleClick}
+    className={mapModifiers('o-tabs_tab', active && 'active')}
+  >
+    <Text modifiers={['20x32', 'sm', 'uppercase', 'white']}>
+      {label}
+    </Text>
+  </div>
+);
+
+const Tabs: React.FC<TabsProps> = ({
+  responsive,
+  children,
+  slidesToShow,
+  tabActive,
+}) => {
+  const settings = {
+    className: 'slider variable-width',
+    arrows: false,
+    dots: false,
+    slidesToShow: slidesToShow || 3,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    variableWidth: true,
+    infinite: false,
+    responsive,
+  };
+
+  const sliderRef = useRef<Slider>(null);
+
+  useEffect(() => {
+    if (sliderRef.current && tabActive) {
+      sliderRef.current.slickGoTo(tabActive);
+    }
+  }, [tabActive, sliderRef]);
+
+  return (
+    <div className="o-tabs">
+      <Slider ref={sliderRef} centerPadding="0" {...settings}>
+        {children}
+      </Slider>
+    </div>
+  );
+};
+
+Tabs.defaultProps = {
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        infinite: true,
+      },
+    },
+  ],
+};
+
+export default Tabs;
