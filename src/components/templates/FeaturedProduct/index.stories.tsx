9a459c5a98ddf2c47d0264a41d3831854d3b2c9c
tiennguyenminh <tien.nguyen@3forcom.com>
2021-08-30 15:06:06 +0700

    :sparkles:intro component

---
+++ src/components/templates/FeaturedProduct/index.stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+
+import FeaturedProduct from '.';
+
+import dummyData from 'assets/dataDummy/featuredProduct';
+
+export default {
+  title: 'Components/templates/FeaturedProduct',
+  component: FeaturedProduct,
+  argTypes: {},
+} as Meta;
+
+export const normal: Story = () => (
+  <div style={{
+    padding: '150px 0',
+  }}
+  >
+    <FeaturedProduct data={dummyData.cardFeatured} />
+  </div>
+);
