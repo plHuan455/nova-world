d71de597809e7038bca798b651d4aa9643ab1824
Chau Truong Phu <chau.truong@3forcom.com.vn>
2021-08-31 07:58:38 +0700

    🎨 Update carousel mobile in ExperienceJourney

--- src/components/templates/ExperienceJourney/index.tsx
+++ src/components/templates/ExperienceJourney/index.tsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
+import Slider from 'react-slick';
 
 import Divider from 'components/atoms/Divider';
-import Text from 'components/atoms/Text';
+import Heading from 'components/atoms/Heading';
 import ExperienceCard, { ExperienceCardProps } from 'components/molecules/ExperienceCard';
+import Carousel, { NextArrow } from 'components/organisms/Carousel';
 import Container from 'components/organisms/Container';
 import useScrollAnimate from 'hooks/useScrollAnimation';
 
@@ -11,37 +13,70 @@ interface ExperienceJourneyProps {
   dataExperienceJourney?: ExperienceCardProps[];
 }
 
+const settingMoblie = {
+  className: 'center',
+  centerMode: true,
+  infinite: true,
+  slidesToShow: 1,
+  speed: 500,
+  rows: 2,
+  slidesPerRow: 2,
+  responsive: [
+    {
+      breakpoint: 575,
+      settings: {
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        nextArrow: <NextArrow variant="large" />,
+        rows: 1,
+        slidesPerRow: 1,
+      },
+    },
+  ],
+};
+
 const ExperienceJourney: React.FC<ExperienceJourneyProps> = ({ title, dataExperienceJourney }) => {
   const ref = useRef<HTMLDivElement>(null);
   const animate = useScrollAnimate(ref);
+  const [nav, setNav] = useState<Slider|null>();
   return (
     <div className="t-expjourney">
       <div className={animate ? 't-expjourney_wrap animate animate-fadeInUp' : 't-expjourney_wrap preanimate'}>
         <Container fullScreen>
           <div className="t-expjourney_title">
-            <Text modifiers={['48x56', '500', 's005', 'center', 'cyanCobaltBlue', 'uppercase']}>
+            <Heading type="h2" modifiers={['500', 's005', 'center', 'cyanCobaltBlue', 'uppercase']}>
               {title}
               <Divider />
-            </Text>
+            </Heading>
           </div>
           <div className="t-expjourney_gallery">
-            {
-              dataExperienceJourney && dataExperienceJourney?.length > 0
-              && dataExperienceJourney?.map((item, index) => (
-                <div
-                  key={`gallery${index.toString()}`}
-                  className="t-expjourney_gallery_item"
-                >
-                  <ExperienceCard
-                    imgSrc={item.imgSrc}
-                    title={item.title}
-                    location={item.location}
-                    stt={index + 1}
-                    href=""
-                  />
-                </div>
-              ))
-            }
+            <Carousel
+              asNavFor={nav as Slider}
+              ref={(slider) => {
+                setNav(slider);
+              }}
+              settings={settingMoblie}
+            >
+              {
+                dataExperienceJourney && dataExperienceJourney?.length > 0
+                && dataExperienceJourney?.map((item, index) => (
+                  <div
+                    key={`gallery${index.toString()}`}
+                    className="t-expjourney_gallery_item"
+                  >
+                    <ExperienceCard
+                      imgSrc={item.imgSrc}
+                      title={item.title}
+                      location={item.location}
+                      stt={index + 1}
+                      href=""
+                    />
+                  </div>
+                ))
+              }
+            </Carousel>
           </div>
         </Container>
       </div>
