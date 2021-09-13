e1f901f1798baec2dd37d3c2567b15ccc81e59d4
tiennguyenminh <tien.nguyen@3forcom.com>
2021-09-13 13:50:20 +0700

    :zap:update

--- src/container/ExperienceJourney/index.tsx
+++ src/container/ExperienceJourney/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 
@@ -10,26 +11,30 @@ const dummyData = [
   {
     imgSrc: 'https://source.unsplash.com/random',
     title: 'Vui chơi giải trí',
-    content: 'Sở hữu hệ sinh thái nghỉ dưỡng đẳng cấp mang phong cách cổ tích như: khách sạn lâu đài Movenpick, hồ bơi vô cực, quảng trường biển,…',
+    content: 'Sở hữu hệ sinh thái nghỉ dưỡng đẳng cấp mang phong cách cổ tích như: khách sạn lâu đài Movenpick, hồ bơi vô cực, quảng trường biển,… ',
     btnLabel: 'Khám phá ngay',
+    btnLink: 'hanh-trinh-trai-nghiem/detail',
   },
   {
     imgSrc: 'https://source.unsplash.com/random',
     title: 'NGHỈ DƯỠNG CAO CẤP',
     content: 'Nằm trọn không gian khí hậu quanh năm nắng ấm và hòa cùng cảnh quan rừng biển Hồ Tràm, những ngôi biệt thự The Tropicana khoác lên mình "chiếc áo" phóng khoáng đặc trưng...',
     btnLabel: 'Khám phá ngay',
+    btnLink: 'hanh-trinh-trai-nghiem/detail',
   },
   {
     imgSrc: 'https://source.unsplash.com/random',
     title: 'DỊCH VỤ LƯU TRÚ',
     content: 'Biệt thự thương mại – Shop Villa có thiết kế công năng kép độc đáo. Tầng 1 với ưu thế căn góc & sân vườn khoáng đạt, được linh hoạt...',
     btnLabel: 'Khám phá ngay',
+    btnLink: 'hanh-trinh-trai-nghiem/detail',
   },
   {
     imgSrc: 'https://source.unsplash.com/random',
     title: 'ẨM THỰC THƯƠNG MẠI',
     content: 'Biệt thự thương mại – Shop Villa có thiết kế công năng kép độc đáo. Tầng 1 với ưu thế căn góc & sân vườn khoáng đạt, được linh hoạt...',
     btnLabel: 'Khám phá ngay',
+    btnLink: 'hanh-trinh-trai-nghiem/detail',
   },
 ];
 
@@ -40,7 +45,7 @@ const ExperienceJourney:React.FC = () => {
       <section>
         <Banner thumbnail="https://source.unsplash.com/random" />
       </section>
-      <section className="s-content s-wrap">
+      <section className="s-wrap">
         <Experience data={dummyData} />
       </section>
     </>
