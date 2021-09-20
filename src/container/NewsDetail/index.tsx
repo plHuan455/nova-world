3f5de94ad8c82aa9ab82b66dde6a1fc0b358c926
Dan To Hoa <dan.to@3forcom.com.vn>
2021-09-20 17:17:23 +0700

    :hammer: hardcode banner

--- src/container/NewsDetail/index.tsx
+++ src/container/NewsDetail/index.tsx
@@ -4,21 +4,19 @@ import { useParams } from 'react-router-dom';
 import Content from './content';
 import Related from './related';
 
+import BannerDetail from 'assets/images/banner_news.jpg';
 import Loading from 'components/atoms/Loading';
 import Banner from 'components/organisms/Banner';
 import HelmetComponent from 'container/MainLayout/helmet';
 import useCallService from 'hooks/useCallService';
 import useMainLayout from 'hooks/useMainLayout';
 import getNewsDetail from 'services/newsDetail';
-import { getImageURL } from 'utils/functions';
 
 const NewsDetail: React.FC = () => {
   useMainLayout('another');
   const { slug } = useParams<{slug?: string}>();
 
   const { status, data } = useCallService(() => getNewsDetail(slug), [slug]);
-
-  console.log(data);
 
   if (status === 'pending') {
     return <Loading modifiers={['blue']} />;
@@ -28,7 +26,7 @@ const NewsDetail: React.FC = () => {
     <>
       <HelmetComponent seoData={data?.seoData} />
       <section className="s-banner">
-        <Banner thumbnail={getImageURL(data?.newsData?.thumbnail)} />
+        <Banner thumbnail={BannerDetail} />
       </section>
       <section className="s-wrap s-donut">
         <Content breadcrumb={data?.breadcrumbs[0]} newsData={data?.newsData} />
