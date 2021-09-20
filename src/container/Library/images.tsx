82092a742ca8c09469d22131e658f75c0048789c
Dan To Hoa <dan.to@3forcom.com.vn>
2021-09-20 19:38:51 +0700

    :hammer: remove comment

--- src/container/Library/images.tsx
+++ src/container/Library/images.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useMemo, useState } from 'react';
 
 import Loading from 'components/atoms/Loading';
