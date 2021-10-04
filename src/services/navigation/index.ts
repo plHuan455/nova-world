662c5767b35100c631f084211a91240ec1e82ccd
Dan To Hoa <dan.to@3forcom.com.vn>
2021-10-04 11:07:42 +0700

    :hammer: chore

--- src/services/navigation/index.ts
+++ src/services/navigation/index.ts
@@ -1,11 +1,11 @@
 import axiosInstance from 'services/common/instance';
 
-export const getStaticHomeService = async <T, >(): Promise<BasePageData<T>> => {
+export const getStaticHomeService = async <T>(): Promise<BasePageData<T>> => {
   const response = await axiosInstance.get('pages/static/home-page');
   return response.data.data;
 };
 
-export const getPageService = async <T, >(slug: string): Promise<BasePageData<T>> => {
+export const getPageService = async <T>(slug: string): Promise<BasePageData<T>> => {
   const response = await axiosInstance.get(`pages/${slug}`);
   return response.data.data;
 };
