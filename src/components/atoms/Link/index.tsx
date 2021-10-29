7206cb726b6510039fbd9e9a531eb6a48d25a3c3
Chau Truong Phu <chau.truong@3forcom.com.vn>
2021-10-29 11:00:59 +0700

    🎨 hotflix(NOVAW-197): Update

--- src/components/atoms/Link/index.tsx
+++ src/components/atoms/Link/index.tsx
@@ -16,7 +16,7 @@ const Link: React.FC<LinkProps> = ({
   state,
   ...props
 }) => {
-  if (href.includes('http')) {
+  if (href && href.includes('http')) {
     return (
       <a {...props} target={target} href={href}>
         {children}
